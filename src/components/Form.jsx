e597f9d13b4a760e146c7b4cd9ede0076e4b095f
TautvydasGec <gecevicius.tautvydas@gmail.com>
2020-10-05 21:38:29 +0200

    Adding conditions for form submition

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,12 +3,13 @@ import React, { useRef, useState } from 'react'
 const Form = ({ onSubmit }) => {
   const nameRef = useRef()
   const scoreRef = useRef()
-  const [showDisclaimer, setShowDisclaimer] = useState(true)
+  const [disclaimer, setDisclaimer] = useState("")
   const handleSubmit = () => {
-    setShowDisclaimer(true)
+    setDisclaimer("")
     const name = nameRef.current.value
     const score = scoreRef.current.value
-    if (!name || !score) return setShowDisclaimer(false)
+    if (!name || !score) return setDisclaimer('Please input both fields')
+    if (score < 0) return setDisclaimer('Select positive value for score')
     onSubmit(name, score)
 
   }
@@ -18,9 +19,9 @@ const Form = ({ onSubmit }) => {
       <input ref={nameRef} type="text" />
       <input ref={scoreRef} type="number" min="0" step="1" />
       <button onClick={handleSubmit}>Submit</button>
-      {!showDisclaimer
+      {disclaimer
         ? <div style={{ color: 'red' }}>
-          Please enter both of the inputs
+          {disclaimer}
         </div>
         : null}
     </div>
